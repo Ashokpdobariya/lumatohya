7d04f6f6c825912d7bed11c3077571c6c95514e5
Ashok_dobariya <ashokpdobariya2081978@gmail.com>
2023-11-14 11:09:02 +0530

    First commmit

---
+++ src/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Zealous Commerce / qtyManager
+ * Copyright © Zealous Commerce. All rights reserved.
+ * This product is licensed per Magento install
+ 
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Hyva_lumatohyva', __DIR__);
